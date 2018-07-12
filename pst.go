fe0fb026272c2d08772b8884d041591fd43ab9cb
Konstantin Sorokin <kvs@sigterm.ru>
2018-07-12 14:51:22 +0300

    code simplification

--- pst.go
+++ pst.go
@@ -191,7 +191,7 @@ func getDigestAuthData(h string) *digestAuthData {
 		s = e + 1
 	}
 
-	tokens = append(tokens, strings.Trim(h[s:len(h)], " "))
+	tokens = append(tokens, strings.Trim(h[s:], " "))
 
 	for _, token := range tokens {
 		kv := strings.SplitN(token, "=", 2)
